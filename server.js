1fd7f2e6e45a4e22b3d995819b6bee04c18a7531
JC <john.crespo@auth0.com>
2021-03-30 18:13:57 +1100

    Stripe integration complete ✅

--- server.js
+++ server.js
@@ -1,53 +1,73 @@
+require("dotenv").config({ path: "./.env" });
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY, { maxNetworkRetries: 3 });
 const express = require("express");
 const app = express();
-const jwt = require("express-jwt");
-const jwtAuthz = require("express-jwt-authz");
-const jwksRsa = require("jwks-rsa");
 const cors = require("cors");
 const morgan = require("morgan");
-var data = require("./data");
-require("dotenv").config();
+const data = require("./data");
 
-if (!process.env.AUTH0_DOMAIN || !process.env.AUTH0_AUDIENCE) {
-  throw "Make sure you have AUTH0_DOMAIN, and AUTH0_AUDIENCE in your .env file";
-}
+app.use(cors({ origin: true }));
 
-app.use(cors());
 app.use(
   morgan(
     "API Request (port " +
-    process.env.PORT +
+    (process.env.PORT || 5000) +
     "): :method :url :status :response-time ms - :res[content-length]"
   )
 );
 
-const checkJwt = jwt({
-  // Dynamically provide a signing key based on the kid in the header and the singing keys provided by the JWKS endpoint.
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
-  }),
-
-  // Validate the audience and the issuer.
-  audience: process.env.AUTH0_AUDIENCE,
-  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
-  algorithms: ["RS256"],
-});
-
-const checkScopes = jwtAuthz(["purchase:products"]);
+app.use(express.json());
 
 app.get("/api/products", function (req, res) {
   res.json({ products: data.pizzas });
 });
 
-app.get("/api/private", checkJwt, checkScopes, function (req, res) {
-  res.json({ message: "response from API: OK ", code: 200 });
+app.post("/api/create_payment_intent", async (req, res) => {
+  const { amount, email } = req.body;
+  const options = {
+    amount: amount,
+    currency: 'aud',
+    metadata: { integration_check: "accept_a_payment" },
+    receipt_email: email
+  };
+  try {
+    const paymentIntent = await stripe.paymentIntents.create(options);
+    res.status(200).send(paymentIntent.client_secret);
+  } catch (error) {
+    res.status(500).json(
+      {
+        statusCode: 500,
+        message: `Something happened! ${error.message}`
+      });
+  }
+});
+
+app.post("/webhooks", async (req, res) => {
+  let data = req.body.data;
+  let eventType = req.body.type;
+
+  // Handle the event
+  switch (eventType) {
+    case 'payment_intent.succeeded':
+      // Fulfill any orders, e-mail receipts, etc
+      console.log(`💰 Payment from ${data.object.receipt_email} received! Total Amount: $${data.object.amount / 100}`);
+      // console.log(`💰 Payment received!`);
+      break;
+    default:
+      console.log(`⚠️ Unexpected event type ${eventType}`);
+      res.status(400).json(
+        {
+          statusCode: 400,
+          message: `Unexpected event type ${eventType}`
+        });
+  }
+
+  // Return a response to acknowledge receipt of the event
+  res.json({ received: true });
 });
 
 app.listen(process.env.PORT || 5000);
 console.log(
   "Server listening on http://localhost:%d. The React app will be built and served at http://localhost:3000.",
-  process.env.PORT
+  process.env.PORT || 5000
 );
